aa7161cf5d5f06f25f03fcfa1bb3eed6660b4c0d
benkeepswalking <benkeepsjourney@gmail.com>
2025-02-09 09:38:25 +0800

    lesson5

---
+++ lesson5/script.js
@@ -0,0 +1,32 @@
+function fetchData(): {
+    // Write your code here
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Data retrieved")
+        }, 1000)
+    })
+    
+    const newPromise = new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve("Data retrieved");
+        },1000);
+    });
+}
+function processData(data: string): Promise<string> {
+    // Write your code here
+    const newPromise2 = new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("Data retrieved" + " and processed");
+        }, 2000);
+        });
+    }
+
+function displayData(data: string) {
+    // Write your code here
+    console.log(data)
+}
+
+// Chaining the functions
+fetchData()
+    .then((data) => processData(data))
+    .then((processedData) => displayData(processedData));
