3be225af10306e9788fc71c9ee977677aa5e011d
benkeepswalking <benkeepsjourney@gmail.com>
2025-02-10 19:31:28 +0800

    Update script.js

--- lesson5/script.js
+++ lesson5/script.js
@@ -1,19 +1,19 @@
-function fetchData(): {
+function fetchData(): Promise<string> {
     // Write your code here
     return new Promise((resolve)=>{
         setTimeout(()=>{
             resolve("Data retrieved")
         }, 1000)
-    })
-    
-    const newPromise = new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve("Data retrieved");
-        },1000);
-    });
-}
-function processData(data: string): Promise<string> {
+    })}
+
+    function processData(data: string): Promise<string> {
     // Write your code here
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve(data + " and processed")
+        }, 2000)
+    })}
+
     const newPromise2 = new Promise((resolve,reject)=>{
         setTimeout(()=>{
             resolve("Data retrieved" + " and processed");
